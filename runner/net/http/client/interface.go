843ec85ef364c9a442f9f669c50a85b6ed333931
soyacen <soyacen@outlook.com>
2022-08-01 16:28:04 +0800

    runner/net/http rm lb logic

--- runner/net/http/client/interface.go
+++ runner/net/http/client/interface.go
@@ -2,14 +2,9 @@ package client
 
 import "context"
 
+// Interface defines the functions clients need to perform RPCs.
+// It is implemented by *Client and *lb.BalancedClient, and is only intended to
+// be referenced by generated code.
 type Interface interface {
 	Invoke(ctx context.Context, method string, path string, in any, out any) error
-}
-
-type InterfaceMapper interface {
-	Interfaces() (map[string]Interface, error)
-}
-
-type InterfaceBalancer interface {
-	Pick(map[string]Interface) (Interface, error)
 }
