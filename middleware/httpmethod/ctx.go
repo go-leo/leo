606dd7b325ee010d74b066f87a6f5021a4f0b597
soyacen <soyacen@outlook.com>
2022-11-07 20:22:20 +0800

    add http method

---
+++ middleware/httpmethod/ctx.go
@@ -0,0 +1,16 @@
+package clientip
+
+import (
+	"context"
+)
+
+type key struct{}
+
+func NewContext(ctx context.Context, method string) context.Context {
+	return context.WithValue(ctx, key{}, method)
+}
+
+func FromContext(ctx context.Context) (method string, ok bool) {
+	method, ok = ctx.Value(key{}).(string)
+	return
+}
---
+++ middleware/httpmethod/http.go
@@ -0,0 +1,15 @@
+package clientip
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GinMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		r := c.Request
+		method := c.Request.Method
+		ctx := r.Context()
+		ctx = NewContext(ctx, method)
+		c.Request = r.WithContext(ctx)
+	}
+}
