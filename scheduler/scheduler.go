5586d24c18991fcc3bceb8fa4b9f5a59ae175a7e
宋延铖 <songyancheng@qimao.com>
2023-02-28 19:27:57 +0800

    feat: add task id and append/remove task at scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -10,6 +10,10 @@ type Scheduler interface {
 	Start(ctx context.Context) error
 	// Stop 停止运行
 	Stop(ctx context.Context) error
+	// AppendTask 追加task
+	AppendTask(task Task) Scheduler
+	// RemoveTask 删除task
+	RemoveTask(task Task) Scheduler
 	// Tasks 任务列表
 	Tasks() []Task
 }
