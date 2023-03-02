9604e90e967d25d7c7f4b97c9d725ba7f7004e5e
宋延铖 <songyancheng@qimao.com>
2023-03-02 18:29:32 +0800

    fix: add error channel at Subscribe return value
    (cherry picked from commit 2ac8d90d0aa6981ba566629a3258bd2e9d5f75c0)

--- stream/subscriber.go
+++ stream/subscriber.go
@@ -5,6 +5,6 @@ import "context"
 // Subscriber is message queue subscriber
 type Subscriber interface {
 	Topic() string
-	Subscribe(ctx context.Context) (<-chan Message, error)
+	Subscribe(ctx context.Context) (<-chan Message, <-chan error)
 	Close() error
 }
