e2e635101af73b3312377fdb1477870dc83a602a
宋延铖 <songyancheng@qimao.com>
2023-03-02 18:01:43 +0800

    fix: rm context arg at publisher.go

--- stream/publisher.go
+++ stream/publisher.go
@@ -1,9 +1,7 @@
 package stream
 
-import "context"
-
 type Publisher interface {
 	Topic() string
-	Publish(ctx context.Context, messages ...Message) (any, error)
+	Publish(messages ...Message) (any, error)
 	Close() error
 }
