b0a9ba23582ac42b9873e535627b91dd3769458d
宋延铖 <songyancheng@qimao.com>
2023-03-02 19:44:41 +0800

    feat: add message transformer interface for stream bridge

--- stream/bridge.go
+++ stream/bridge.go
@@ -1,7 +1,20 @@
 package stream
 
+import "context"
+
 type Bridge interface {
 	Input() Subscriber
 	Output() Publisher
-	Process(msg Message) (Message, error)
+	MessageTransformer() MessageTransformer
+	Process(ctx context.Context)
+}
+
+type MessageTransformer interface {
+	Transform(msg Message) (Message, error)
+}
+
+type MessageTransformerFunc func(msg Message) (Message, error)
+
+func (m MessageTransformerFunc) Transform(msg Message) (Message, error) {
+	return m(msg)
 }
