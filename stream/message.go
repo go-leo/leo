fabaf0e871b4abff17066b789243799b8ffd3f2e
宋延铖 <songyancheng@qimao.com>
2023-03-02 16:35:57 +0800

    feat: add ack and nack to stream message

--- stream/message.go
+++ stream/message.go
@@ -8,9 +8,22 @@ type Header interface {
 }
 
 type Message interface {
+	// ID return message uniq id
 	ID() string
+	// Payload return message payload
 	Payload() []byte
+	// Header return message header
 	Header() Header
+	// Context return message context
 	Context() context.Context
+	// WithContext return new message with context
 	WithContext(ctx context.Context) Message
+	// Ack ack message
+	Ack() bool
+	// Nack nack message
+	Nack() bool
+	// Acked returns channel which is closed when acknowledgement is sent.
+	Acked() <-chan struct{}
+	// Nacked returns channel which is closed when negative acknowledgement is sent.
+	Nacked() <-chan struct{}
 }
