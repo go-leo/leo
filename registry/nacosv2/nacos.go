820c6429633be869a842eb88d3ad58de30ad6dca
宋延铖 <songyancheng@qimao.com>
2023-03-22 16:43:29 +0800

    add NamingClientFactoryFunc for create nacos INamingClient

--- registry/nacosv2/nacos.go
+++ registry/nacosv2/nacos.go
@@ -1,7 +1,15 @@
 package nacosv2
 
-import "github.com/nacos-group/nacos-sdk-go/v2/clients/naming_client"
+import (
+	"github.com/nacos-group/nacos-sdk-go/v2/clients/naming_client"
+)
 
 type NamingClientFactory interface {
 	Create() naming_client.INamingClient
+}
+
+type NamingClientFactoryFunc func() naming_client.INamingClient
+
+func (f NamingClientFactoryFunc) Create() naming_client.INamingClient {
+	return f()
 }
