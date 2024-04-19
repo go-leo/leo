0751141261416b696416cf4bb9b98b9679b5966c
soyacen <soyacen@outlook.com>
2024-04-19 11:18:35 +0800

    feat: add runner.RunnerFunc

--- runner/runner.go
+++ runner/runner.go
@@ -9,3 +9,9 @@ type Runner interface {
 	// Run 启动
 	Run(ctx context.Context) error
 }
+
+type RunnerFunc func(ctx context.Context) error
+
+func (f RunnerFunc) Run(ctx context.Context) error {
+	return f(ctx)
+}
