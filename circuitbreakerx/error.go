e6a1ff3cf29cc505f0535b3966f53e2104c81561
soyacen <soyacen@outlook.com>
2025-03-24 09:28:10 +0800

    feat: ErrCircuitOpen add Identifier

--- circuitbreakerx/error.go
+++ circuitbreakerx/error.go
@@ -7,7 +7,10 @@ import (
 var (
 	// ErrCircuitOpen is returned by the transport when the downstream is
 	// unavailable due to a broken circuit.
-	ErrCircuitOpen = statusx.Unavailable(statusx.Message("circuitbreakerx: circuit breaker is open"))
+	ErrCircuitOpen = statusx.Unavailable(
+		statusx.Message("circuitbreakerx: circuit breaker is open"),
+		statusx.Identifier("github.com/go-leo/leo/v3/circuitbreakerx.ErrCircuitOpen"),
+	)
 
 	errLoadBreaker = "circuitbreakerx: failed to load %s breaker"
 )
